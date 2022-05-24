c8ea8c105f077eb5b25c84d66e2d8cdc5e60b25a
obzva <dngyng1000@gmail.com>
2022-05-24 15:08:41 +0900

    220524 4 added row text

--- src/components/row/row.js
+++ src/components/row/row.js
@@ -5,9 +5,9 @@ const Row = () => {
   return (
     <div className="rowWrapper">
       <div className="rowTextWrapper">
-        <p className='text1 contentTitle'>당신이 먹는 생수,</p>
-        <p className='text2 mainTitle'>어떤 브랜드인가요?</p>
-        <p className='text3 mainSubTitle'>(수질검사 통과 못한게 많다는 sub text)</p>
+        <p className='rowText1 contentTitle'>당신이 먹는 생수,</p>
+        <p className='rowText2 mainTitle'>어떤 브랜드인가요?</p>
+        <p className='rowText3 mainSubTitle rowGrayTypo'>(수질검사 통과 못한게 많다는 sub text)</p>
       </div>
     </div>
   );

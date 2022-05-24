8a82d6c16722fe80eaccd75947b8c5634d1cf354
obzva <dngyng1000@gmail.com>
2022-05-24 14:42:34 +0900

    220524 3 fixed intro classname

--- src/components/intro.js
+++ src/components/intro.js
@@ -4,17 +4,17 @@ import "./intro.css";
 const Intro = () => {
   return (
     <div className="introWrapper">
-      <p className="contentTitle text1">당신이 먹는 생수,</p>
-      <p className="mainTitle text2">알고 드시나요?</p>
-      <p className="mainSubTitle text3 grayTypo">
+      <p className="contentTitle introText1">당신이 먹는 생수,</p>
+      <p className="mainTitle introText2">알고 드시나요?</p>
+      <p className="mainSubTitle introText3 introGrayTypo">
         (수질검사 통과 못한게 많다는 텍스트)
       </p>
-      <div className='img1'></div>
-      <div className='img2'></div>
-      <p className ='text4 mainSubTitle'>{'국내 생수 브랜드는\n40가지가 넘지만,'}</p>
-      <p className ='text5 mainSubTitle'>{'생수 제조원은\n6개밖에 되지 않아요!'}</p>
-      <div className='img3'></div>
-      <div className='img4'></div>
+      <div className='introImg1'></div>
+      <div className='introImg2'></div>
+      <p className ='introText4 mainSubTitle'>{'국내 생수 브랜드는\n40가지가 넘지만,'}</p>
+      <p className ='introText5 mainSubTitle'>{'생수 제조원은\n6개밖에 되지 않아요!'}</p>
+      <div className='introImg3'></div>
+      <div className='introImg4'></div>
     </div>
   );
 };
--- src/components/row/row.js
+++ src/components/row/row.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import './row.css';
+import React from "react";
+import "./row.css";
 
 const Row = () => {
   return (
-    <div className='rowWrapper'></div>
-  )
-}
+    <div className="rowWrapper">
+      <div className="rowTextWrapper">
+        <p className='text1 contentTitle'>당신이 먹는 생수,</p>
+        <p className='text2 mainTitle'>어떤 브랜드인가요?</p>
+        <p className='text3 mainSubTitle'>(수질검사 통과 못한게 많다는 sub text)</p>
+      </div>
+    </div>
+  );
+};
 
-export default Row
+export default Row;
